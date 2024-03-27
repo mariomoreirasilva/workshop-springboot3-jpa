7e9cb08ea7777d0e58d1e088bfa52f8de4eaa0e0
mariomoreirasilva <mario@desban.org.br>
2024-03-27 11:22:00 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,21 +1,41 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 //recursos rest da aplicação
 @RestController
 @RequestMapping("/users")
 public class UserResource {
+	//agora vamos implementar a injeção de pepdendencia
+	@Autowired
+	private UserService service;
+	
     //o metodo vai responder pelo get do htt anotation abaixo
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		//testar a rquisição web
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "123456");
-		return ResponseEntity.ok().body(u);
+		//User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "123456");
+		//return ResponseEntity.ok().body(u);
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		
 	}
+	//parâmetro na URL do id
+	@GetMapping(value = "/{id}")		//anotarion para o spring saber que esta vindo da url
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+	
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+//declarar como se fosse um componente de serviço para o resource dar certo
+@Service
+public class UserService {
+	
+	//tem que ter dependencia do Repository. 
+	@Autowired //faz a injeção de dependencia automática sem o programador ver
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
